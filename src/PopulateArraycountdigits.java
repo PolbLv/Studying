0d80448e54853e780e10a3deebad7eabefabdbca
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-03-24 15:32:38 +0200

    Update

--- src/PopulateArraycountdigits.java
+++ src/PopulateArraycountdigits.java
@@ -3,7 +3,7 @@
  */
 public class PopulateArraycountdigits {
     public static void main(String[] args) {
-        int[] array = populateArray(5, 500);
+        int[] array = populateArray(10, 500);
         countArray(array);
     }
 
@@ -12,7 +12,7 @@ public class PopulateArraycountdigits {
         int[] array = new int[size];
         for (int i = 0; i < size; i++) {
             array[i] = (int) (Math.random() * (maxRandomNumber + 1));
-            System.out.println(array[i]);
+ //          System.out.println(array[i]);
         }
         return array;
     }
@@ -20,13 +20,12 @@ public class PopulateArraycountdigits {
     public static void countArray(int[] array ) {
         int counter= 0;
         for (int i = 0; i <array.length; i++) {
-            if (i % 2 ==0)
+            if (array[i] % 2 ==0)
                 counter++;
         }
         System.out.println(counter);
     }
 }
-
 //Используя метод populateArray создать массив чисел от 0 до 500, вывести на экран количество четных чисел в массиве
 
 /*1) метод мэйн, точка входа в твою программу, из этого метода ты вызываешь оба других метода
