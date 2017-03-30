74ac69c66a099e0f0ac0b6565ccb81ebc54af344
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-03-30 18:14:16 +0300

    Update

--- src/Tasks_for_loops2017_03_30/ZeroToHundredUniversalWay.java
+++ src/Tasks_for_loops2017_03_30/ZeroToHundredUniversalWay.java
@@ -4,13 +4,47 @@ package Tasks_for_loops2017_03_30;
  * Created by LVIVSOFT\spolyakov on 30.03.17.
  */
 public class ZeroToHundredUniversalWay {
-    public static void main (String arg[]){
+    public static void main(String arg[]) {
         ZeroToHundred(0, 100);
+        minusHundredToZero(-100, 0);
+        evenZeroTohundred(0, 100);
+        minusOddHundredToZero( -100, 0);
     }
 
-    public static void ZeroToHundred(int min, int max){
-        for (int i = min; i <= max; i++){
+    public static void ZeroToHundred(int min, int max) {
+        for (int i = min; i <= max; i++) {
             System.out.println("ZeroToHundred " + i);
         }
     }
+
+    public static void minusHundredToZero(int min, int max) {
+        for (int i = min; i <= max; i++) {
+            System.out.println("minusHundredToZero " + i);
+        }
+    }
+
+    public static void evenZeroTohundred(int min, int max) {
+        for (int i =min; i <= max; i++) {
+            if (i % 2 == 0) {
+                System.out.println("evenZeroToHundred " + i);
+            }
+        }
+
+    }
+
+    public static void minusOddHundredToZero(int min, int max) {
+        for (int i = min; i <= max; i++) {
+            if (i % 2 != 0) {
+                System.out.println("minusOddHundredToZero " + i);
+            }
+        }
+    }
 }
+
+
+
+
+
+/*    Написать программу выводящую на экран числа от 0 до 100,
+затем числа от -100 до 0 затем четные числа в диапазоне от 0 до 100,
+        затем нечетные числа в диапазоне от -100 до 0*/
