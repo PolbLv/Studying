f14d2943c6425e9f412710cd4e2eecaf585989a5
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-03-30 18:16:55 +0300

    Update

--- src/Tasks_for_loops2017_03_30/ZeroToHundredCrossFunctionalWay.java
+++ src/Tasks_for_loops2017_03_30/ZeroToHundredCrossFunctionalWay.java
@@ -3,7 +3,7 @@ package Tasks_for_loops2017_03_30;
 /**
  * Created by LVIVSOFT\spolyakov on 30.03.17.
  */
-public class ZeroToHundredUniversalWay {
+public class ZeroToHundredCrossFunctionalWay {
     public static void main(String arg[]) {
         ZeroToHundred(0, 100);
         minusHundredToZero(-100, 0);
