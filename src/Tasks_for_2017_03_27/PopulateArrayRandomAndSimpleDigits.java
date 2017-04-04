9c31c2e97c6311a4301deb6590d08f4840c49dd9
Serhiy Polyakov <s.polb.lv@gmail.com>
2017-04-05 01:34:05 +0300

    Update

--- src/Tasks_for_2017_03_27/PopulateArrayRandomAndSimpleDigits.java
+++ src/Tasks_for_2017_03_27/PopulateArrayRandomAndSimpleDigits.java
@@ -7,19 +7,32 @@ import java.util.ArrayList;
  */
 public class PopulateArrayRandomAndSimpleDigits {
     public static void main(String args[]){
+        int counter = 0;
+        ArrayList<Integer> myList = populateList();
+        for (int i =0; i < myList.size() ; i++){
+            if(isSimple(myList.get(i))){
+                counter++;
+            }
+        }
+        System.out.println(myList);
+            System.out.println(counter);
+        //System.out.println(0/0);
 
     }
     public static ArrayList<Integer> populateList() {
         ArrayList<Integer> myList = new ArrayList<>();
         for (int i = 0; i < 13; i++) {
-            myList.add((int) (Math.random() * (500 + 1)));
+            myList.add((int) (Math.random() * (10 + 1)));
         }
         return myList;
     }
 
     public static boolean isSimple (int num) {
+        if(num==0){
+            return false;
+        }
         for (int x = 2; x < num; x++) {
-            if (num % x == 0) {
+            if (num % x == 0 ) {
                 return false;
             }
         }
