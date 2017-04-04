8e009bb7fe38eb39273a1d35a24f393d0af2300f
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-04-04 15:42:00 +0300

    Update

--- src/Task_for_2017_03_31/SumFirstAndLastNumber.java
+++ src/Task_for_2017_03_31/SumFirstAndLastNumber.java
@@ -4,14 +4,15 @@ package Task_for_2017_03_31;
 public class SumFirstAndLastNumber {
     public static void main(String args[]) {
 
-        sumNumbers(1775);
+        sumNumbers(4561);
     }
 
     public static int sumNumbers(int x) {
         String number = String.valueOf(x);
         for (int i = 0; i < number.length(); i++) {
-            int j = Character.digit(number.charAt(Integer), 10);
-            System.out.println("digit: " + j);
+            int first = x / 1000;
+            int last = x % 10;
+            System.out.println("digit: " +  (last+first));
         }
         return x;
     }
