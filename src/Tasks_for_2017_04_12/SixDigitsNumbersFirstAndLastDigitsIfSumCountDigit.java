cad70d0c61663acee28aa4d80b28040452cc4cb6
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-04-13 16:13:34 +0300

    Update

--- src/Tasks_for_2017_04_12/SixDigitsNumbersFirstAndLastDigitsIfSumCountDigit.java
+++ src/Tasks_for_2017_04_12/SixDigitsNumbersFirstAndLastDigitsIfSumCountDigit.java
@@ -3,12 +3,18 @@ package Tasks_for_2017_04_12;
 import java.util.ArrayList;
 
 public class SixDigitsNumbersFirstAndLastDigitsIfSumCountDigit {
-    public static void main(String args[]){
-    for (int i = 0; i < 100000; i++){
-        System.out.println(i);
+    public static void main(String args[]) {
+        ArrayList<Integer> sum;
 
-    }
+        for (int i = 0; i < 100000; i++) {
+            if (isSimple(i) ) {
+                sum = getDigitsOfNumber(i);
+                if ((sum.get(0) + sum.get(sum.size() - 1)) % 2 == 0){
 
+                    System.out.println(i);
+                }
+            }
+        }
     }
 
     public static ArrayList<Integer> getDigitsOfNumber(int num) {
@@ -19,5 +25,18 @@ public class SixDigitsNumbersFirstAndLastDigitsIfSumCountDigit {
         }
         return digits;
     }
+
+    public static boolean isSimple(int num) {
+        if (num == 0 || num == 1) {
+            return false;
+        }
+        for (int x = 2; x < num; x++) {
+            if (num % x == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
+//Вывести все простые числа от 0 до 100000 у которых сумма первой и последней цифры четное число
