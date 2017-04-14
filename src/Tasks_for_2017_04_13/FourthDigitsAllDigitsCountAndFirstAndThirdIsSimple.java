b9ea1b4c757e550e82eac3899bffe96bf3348ce0
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-04-14 16:45:56 +0300

    Update

--- src/Tasks_for_2017_04_13/FourthDigitsAllDigitsCountAndFirstAndThirdIsSimple.java
+++ src/Tasks_for_2017_04_13/FourthDigitsAllDigitsCountAndFirstAndThirdIsSimple.java
@@ -6,19 +6,20 @@ import java.util.ArrayList;
  * Created by LVIVSOFT\spolyakov on 14.04.17.
  */
 public class FourthDigitsAllDigitsCountAndFirstAndThirdIsSimple {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         int numbers;
         int sum;
         for (int i = 1000; i < 10000; i++) {
             sum = getDigitsOfNumber(i).get(0) + getDigitsOfNumber(i).get(1) + getDigitsOfNumber(i).get(2) + getDigitsOfNumber(i).get(3);
             numbers = getDigitsOfNumber(i).get(0) * 10 + getDigitsOfNumber(i).get(2);
-            if (sum % 2 == 0 && isSimple(numbers))  {
+            if (sum % 2 == 0 && isSimple(numbers)) {
 
-                System.out.println("i = " + i + " sum = " + sum + " isSimple = " + numbers );
+                System.out.println("i = " + i + " sum = " + sum + " isSimple = " + numbers);
             }
 
         }
     }
+
     public static ArrayList<Integer> getDigitsOfNumber(int num) {
         ArrayList<Integer> digits = new ArrayList<>();
         while (num > 0) {
@@ -27,6 +28,7 @@ public class FourthDigitsAllDigitsCountAndFirstAndThirdIsSimple {
         }
         return digits;
     }
+
     public static boolean isSimple(int num) {
         if (num == 0) {
             return false;
---
+++ src/Tasks_for_2017_04_13/FourthDigitsAllNumbersWithDigitFour.java
@@ -0,0 +1,43 @@
+package Tasks_for_2017_04_13;
+
+import java.util.ArrayList;
+
+
+public class FourthDigitsAllNumbersWithDigitFour {
+    public static void main(String args[]) {
+
+        int counter = 0;
+        for (int i = 10000; i < 100000; i++) {
+            if (numbersWithDigitFour((i))) {
+                System.out.println("i = " + i + " counter = " + counter);
+                counter++;
+            }
+        }
+    }
+
+
+    public static boolean numbersWithDigitFour(int x) {
+        ArrayList<Integer> var = getDigitsOfNumber(x);
+        for (int i = 0; i <var.size(); i++) {
+            if (var.get(i) == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<Integer> getDigitsOfNumber(int num) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (num > 0) {
+            digits.add(0, num % 10);
+            num /= 10;
+        }
+        return digits;
+    }
+}
+
+/* Вывести все числа в диапазоне 1000-10000 в которых присутствуют цифры "4".
+Подсчитать количество цифр и вывести их на экран. Примеры:
+1011 - не выводим ничего
+4000 - присутсвует циферка 4 - выводим на экран "4000 - 1" - где цифра "1" - количество найденых цифр в начальном числе
+3440 - присутсвует циферка 4 - выводим на экран "3440 - 2" - где цифра "2" - количество найденых цифр в начальном числе*/
