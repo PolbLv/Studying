df6da21ae55a1ba7625e9ca451298c4a08c17092
Serhiy Polyakov <s.polb.lv@gmail.com>
2017-04-16 19:52:40 +0300

    Update

--- src/Tasks_for_2017_04_13/EightThousandDigitsOfLeftEqualRight.java
+++ src/Tasks_for_2017_04_13/EightThousandDigitsOfLeftEqualRight.java
@@ -7,13 +7,21 @@ public class EightThousandDigitsOfLeftEqualRight {
     public static void main(String args[]) {
         int sum1;
         int sum2;
+        int counter = 0;
         for (int i = 10000000; i < 100000000; i++) {
             sum1 = getDigitsOfNumber(i).get(0) + getDigitsOfNumber(i).get(1) + getDigitsOfNumber(i).get(2) + getDigitsOfNumber(i).get(3);
             sum2 = getDigitsOfNumber(i).get(4) + getDigitsOfNumber(i).get(5) + getDigitsOfNumber(i).get(6) + getDigitsOfNumber(i).get(7);
             if (sum1 == sum2) {
-                System.out.println(" i = " + i + " Left =  " + sum1 + " Right = " + sum2);
+                counter++;
+                System.out.println("number " + counter + ": " + i + " Left =  " + sum1 + " Right = " + sum2);
+
+
             }
+            if(counter == 1000)
+                break;
+
         }
+
     }
 
     public static ArrayList<Integer> getDigitsOfNumber(int num) {
@@ -27,4 +35,4 @@ public class EightThousandDigitsOfLeftEqualRight {
 }
 
 /*Вывести тысячу 8 значных чисел в которых сумма цифр справа равна сумме цифр слева. Например:
-12303300 - 1 + 2 + 3 +0 = 6 ; 3+3+0+0 = 6 - суммы одинаковы, выводим на экран*/
+12303300 - 1 + 2 + 3 + 0 = 6 ; 3+3+0+0 = 6 - суммы одинаковы, выводим на экран*/
