9fc0f7e161ab774863de60434dfd76553ac7b664
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-04-28 15:40:41 +0300

    Update

--- src/Task_for_2017_04_26/TenToHundredSumMeasures.java
+++ src/Task_for_2017_04_26/TenToHundredSumMeasures.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 
 public class TenToHundredSumMeasures {
     public static void main(String args[]) {
-        myMethod(0, 100);
+        myMethod(10, 100);
     }
-
     public static void myMethod(int x, int y) {
 
         ArrayList<Integer> digits;
-
-        int sum =0;
+        int sum;
         for (int i = x; i < y; i++) {
             digits = getDenominatorsOf(i);
-            //sum = sum + digits;
-
+            sum = 0;
+            for (int j = 0; j< digits.size(); j++){
+                sum = sum + digits.get(j);
+            }
             System.out.println("i = " + i + " result = " + sum + "digits = " + digits );
         }
     }
@@ -31,6 +31,4 @@ public class TenToHundredSumMeasures {
         return digits;
     }
 }
-
-
 /*Для чисел от 10 до 100 вывести для каждого сумму всех его делителей*/
